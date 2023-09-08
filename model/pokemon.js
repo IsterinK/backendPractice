12ee5c13ea98fab33f15231392807b876d509176
Esteban Patiño <esteban.pg45@gmail.com>
2023-09-08 16:51:08 -0500

    Movie and Pokemon models

---
+++ model/pokemon.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const pokemonSchema=mongoose.Schema({
+    abilities: {type : Array},
+    base_experience: {type: Number},
+    forms: {type: Array},
+    game_indices: {type:Array},
+    height: { type: Number },
+    held_items: { type: Array },
+    id: { type: Number },
+    is_default:{ type: Boolean },
+    moves: { type: Array },
+    name: { type: String },
+    order: { type: Number },
+    species:{ type: Object },
+    weight: {type: Number}
+})
+
+module.exports = mongoose.model("Pokemon", pokemonSchema)
