d7cd8a54cd787b48ced8c9dfdfd2f3c877aa2869
santiagojimenezv <s.jimenezv10@gmail.com>
2023-10-25 22:40:55 -0500

    corchete

--- model/user.js
+++ model/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, require: true },
     lastname: { type: String, require: true },
     active: { type: Boolean,  default: true },
-    avatar: { type: String, default: "https://www.iconarchive.com/download/i96797/iconsmind/outline/Post-Mail-2.ico",
+    avatar: { type: String, default: "https://www.iconarchive.com/download/i96797/iconsmind/outline/Post-Mail-2.ico"},
     email: { type: String, require: true, unique: true},
     password: { type: String, require: true },
     documentType: { type: String, require: true },
